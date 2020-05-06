bf4346be35894ebc74ace4fad41a5c04773ec31e
Steven Peters <scpeters@openrobotics.org>
2020-05-06 14:42:13 -0700

    Add test for macros with multiple arguments (#69)

--- test/console_TEST.cc
+++ test/console_TEST.cc
@@ -25,3 +25,11 @@ TEST(ConsoleTest, MacroExpansionTest_ItShouldCompile)
     CONSOLE_BRIDGE_logDebug("Testing Log");
   }
 }
+
+//////////////////////////////////////////////////
+TEST(ConsoleTest, MultipleArguments)
+{
+  CONSOLE_BRIDGE_logError("no extra parameters");
+  CONSOLE_BRIDGE_logError("one integer: %d", 42);
+  CONSOLE_BRIDGE_logError("two floats: %f, %.2f", 42.01, 1/3.0);
+}
