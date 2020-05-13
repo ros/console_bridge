7851616c97ada75dfc5c1c0ba8bdbd955ac342c3
ahcorde <ahcorde@gmail.com>
2020-05-13 12:16:33 +0200

    Added features and improved tests
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- test/console_TEST.cc
+++ test/console_TEST.cc
@@ -3,33 +3,98 @@
 
 #include <console_bridge/console.h>
 
-//////////////////////////////////////////////////
-TEST(ConsoleTest, MacroExpansionTest_ItShouldCompile)
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
+std::vector<std::string> split(const std::string& str, char delim = '\n')
 {
-  if (true)
-    CONSOLE_BRIDGE_logDebug("Testing Log");
+    std::vector<std::string> result;
+    std::stringstream ss(str);
+    std::string token;
+    while (std::getline(ss, token, delim)) {
+        result.push_back(token);
+    }
+    return result;
+}
 
-  if (true)
-    CONSOLE_BRIDGE_logDebug("Testing Log");
-  else
-  {
-      assert(true);
-  }
+//////////////////////////////////////////////////
+TEST(ConsoleTest, TestLogLevel)
+{
+  console_bridge::setLogLevel(console_bridge::CONSOLE_BRIDGE_LOG_DEBUG);
+  EXPECT_EQ(console_bridge::getLogLevel(), console_bridge::CONSOLE_BRIDGE_LOG_DEBUG);
 
-  if (true)
-  {
-    CONSOLE_BRIDGE_logDebug("Testing Log");
-  }
-  else
-  {
-    CONSOLE_BRIDGE_logDebug("Testing Log");
-  }
+  console_bridge::setLogLevel(console_bridge::CONSOLE_BRIDGE_LOG_INFO);
+  EXPECT_EQ(console_bridge::getLogLevel(), console_bridge::CONSOLE_BRIDGE_LOG_INFO);
+
+  console_bridge::setLogLevel(console_bridge::CONSOLE_BRIDGE_LOG_WARN);
+  EXPECT_EQ(console_bridge::getLogLevel(), console_bridge::CONSOLE_BRIDGE_LOG_WARN);
+
+  console_bridge::setLogLevel(console_bridge::CONSOLE_BRIDGE_LOG_ERROR);
+  EXPECT_EQ(console_bridge::getLogLevel(), console_bridge::CONSOLE_BRIDGE_LOG_ERROR);
+
+  console_bridge::setLogLevel(console_bridge::CONSOLE_BRIDGE_LOG_NONE);
+  EXPECT_EQ(console_bridge::getLogLevel(), console_bridge::CONSOLE_BRIDGE_LOG_NONE);
 }
 
 //////////////////////////////////////////////////
 TEST(ConsoleTest, MultipleArguments)
 {
+  console_bridge::setLogLevel(console_bridge::CONSOLE_BRIDGE_LOG_DEBUG);
+
+  // This can be an ofstream as well or any other ostream
+  std::stringstream buffer;
+  // Save cout's buffer here
+  std::streambuf *sbuf = std::cerr.rdbuf();
+  // Redirect cerr to our stringstream buffer or any other ostream
+  std::cerr.rdbuf(buffer.rdbuf());
+
+  // get cout
+  testing::internal::CaptureStdout();
+
   CONSOLE_BRIDGE_logError("no extra parameters");
+  CONSOLE_BRIDGE_logDebug("no extra parameters");
+  std::string output_debug = testing::internal::GetCapturedStdout();
+  std::vector<std::string> output_error = split(buffer.str(), '\n');
+  EXPECT_STREQ("Debug:   no extra parameters\n", output_debug.c_str());
+  EXPECT_EQ(output_error.size(), 2u);
+  EXPECT_STREQ("Error:   no extra parameters", output_error[0].c_str());
+
+  // get cout
+  testing::internal::CaptureStdout();
+  buffer.str("");
   CONSOLE_BRIDGE_logError("one integer: %d", 42);
+  CONSOLE_BRIDGE_logDebug("one integer: %d", 42);
+  output_error = split(buffer.str(), '\n');
+  EXPECT_EQ(output_error.size(), 2u);
+  output_debug = testing::internal::GetCapturedStdout();
+  EXPECT_STREQ("Debug:   one integer: 42\n", output_debug.c_str());
+  EXPECT_STREQ("Error:   one integer: 42", output_error[0].c_str());
+
+  console_bridge::setLogLevel(console_bridge::CONSOLE_BRIDGE_LOG_ERROR);
+  // get cout
+  testing::internal::CaptureStdout();
+  buffer.str("");
   CONSOLE_BRIDGE_logError("two floats: %f, %.2f", 42.01, 1/3.0);
+  CONSOLE_BRIDGE_logDebug("two floats: %f, %.2f", 42.01, 1/3.0);
+  output_error = split(buffer.str(), '\n');
+  EXPECT_EQ(output_error.size(), 2u);
+  output_debug = testing::internal::GetCapturedStdout();
+  EXPECT_STREQ("", output_debug.c_str());
+  EXPECT_STREQ("Error:   two floats: 42.010000, 0.33", output_error[0].c_str());
+
+  console_bridge::setLogLevel(console_bridge::CONSOLE_BRIDGE_LOG_NONE);
+  // get cout
+  testing::internal::CaptureStdout();
+  buffer.str("");
+  CONSOLE_BRIDGE_logError("two floats: %f, %.2f", 42.01, 1/3.0);
+  CONSOLE_BRIDGE_logDebug("two floats: %f, %.2f", 42.01, 1/3.0);
+  output_error = split(buffer.str(), '\n');
+  EXPECT_EQ(output_error.size(), 0u);
+  output_debug = testing::internal::GetCapturedStdout();
+  EXPECT_STREQ("", output_debug.c_str());
+
+  // When done redirect cout to its old self
+  std::cerr.rdbuf(sbuf);
 }
